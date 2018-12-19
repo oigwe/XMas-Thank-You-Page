14025634528cfd1d1309793a3bf5a7dd120a7467
ositaigwe@pursuit.org <j0h@example.com>
2018-12-19 10:28:45 -0500

    adds node

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
-import commentBox from 'commentbox.io'; // also available on CDN
+
+const commentBox = require('commentbox.io');
+// or if using the CDN, it will be available as a global "commentBox" variable.
+
 commentBox('my-project-id');
